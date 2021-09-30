7c6c871f0264c8431c3e1d642b30f861c9292856
Carlos <robocarlos10@gmail.com>
2021-09-30 20:36:12 +0200

    finalizacion de EJercicio 5

--- src/accesodatos/Tema1/Ejercicios/Ejercicio5.java
+++ src/accesodatos/Tema1/Ejercicios/Ejercicio5.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package accesodatos.Tema1.Ejercicios;
 
 import java.io.BufferedReader;
@@ -11,38 +7,35 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- *
  * @author Manuel
+ * Este es un programa al que se le pasa una ruta como parametro y lee y escribe por pantalla cada una de las lineas del archivo
  */
 public class Ejercicio5 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException{
         String ruta = args[0];
         File archivo = new File(ruta);
-        FileReader fr = null;
-        BufferedReader br = null;
-        
-       try {
-         fr = new FileReader (archivo);
-         br = new BufferedReader(fr);
-         String linea;
-         while((linea=br.readLine())!=null)
-            System.out.println(linea);
-      }
-      catch(Exception e){
-         e.printStackTrace();
-      }finally{
-         try{                    
-            if( null != fr ){   
-               fr.close();     
-            }                  
-         }catch (Exception e2){ 
-            e2.printStackTrace();
-         }
-      }
-    }
+        FileReader fr = new FileReader(archivo);
+	BufferedReader br = new BufferedReader(fr);
+        String texto;
 
+        try {
+            if (args.length == 0 || args.length > 1) {
+                System.out.println("Los Argumentos no son validos");
+            } else {
+                while ((texto = br.readLine()) != null) {
+                    System.out.println(texto);
+                }
+                fr.close();
+                br.close();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Los Argumentos no son validos" + e.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
